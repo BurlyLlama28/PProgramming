ad3ace1b07f243de9e5ad30b4a29ba6fea59f9fb
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-12-26 15:32:23 +0200

    small fixes

--- config.py
+++ config.py
@@ -4,16 +4,12 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity
 )
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '4F4A8B8690219C1841B865EB87E8EC40281F7784BA16AEF0408DC712A6F3B4D7'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
-    user='root',
-    password='olik1003',
-    server='localhost',
-    database='cinema_db'
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("URL")
 
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
